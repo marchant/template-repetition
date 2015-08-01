26d9d0b74bcfac0e9e4fe582aaccf2d8636ea3ae
Benoit Marchant <benoit@montagestudio.com>
2015-08-01 16:14:06 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -12,5 +12,34 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+    data : {
+        value: [
+        {
+            "make": "Toyota",
+            "model": "Camry",
+            "modelYear": 2009,
+            "licensePlate": "GA39 150543"
+        },
+        {
+            "make": "Honda",
+            "model": "Civic",
+            "modelYear": 2014,
+            "licensePlate": "B890 652511"
+        },
+        {
+            "make": "Acura",
+            "model": "RDX",
+            "modelYear": 2016,
+            "licensePlate": "XC05 355523"
+        },
+        {
+            "make": "Nissan",
+            "model": "Leaf",
+            "modelYear": 2015,
+            "licensePlate": "KA09 650543"
+        }
+        ]   
     }
+
 });
